5c30d5dd7eb7e3cdeac77c0097e2ea09d3946599
MAHAM42 <161121868+MAHAM42@users.noreply.github.com>
2024-04-24 11:01:47 -0700

    5th-Assignment
    TypeScript 45-Assignment of Q5 with Node.js

---
+++ main5.js
@@ -0,0 +1,12 @@
+"use strict";
+// ASSIGNMENT NO O5:
+/*
+repeat Exercise 4, but this time store the famous person's name in a variable called
+famous_peron. then compose your message and store it in a new variable called message. Print
+your message.
+*/
+console.log("ASSIGNMENT NO 05");
+const famousperson = "Albert Einstein";
+const message = `${famousperson} once said, "A person  who never made a mistake never
+tried anything new."`;
+console.log(message);
---
+++ main5.ts
@@ -0,0 +1,16 @@
+// ASSIGNMENT NO O5:
+
+/*
+repeat Exercise 4, but this time store the famous person's name in a variable called 
+famous_peron. then compose your message and store it in a new variable called message. Print
+your message.
+*/
+
+console.log("ASSIGNMENT NO 05");
+
+const famousperson: string = "Albert Einstein";
+
+const message: string = `${famousperson} once said, "A person  who never made a mistake never
+tried anything new."`;
+
+console.log(message); 
